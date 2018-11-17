e2d534243b1e1e3725d0e9c7b503883801538ebb
Paulo Eugenio <pauloeugenioreis@msn.com>
2018-11-17 18:50:18 -0400

    alinhamento

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AppVitrine from './components/Vitrine/App';
 
-ReactDOM.render(<AppVitrine api="/json/challenge.json" />, document.getElementById('container'));
+ReactDOM.render(<AppVitrine api='/json/challenge.json'/>, document.getElementById('container'));
