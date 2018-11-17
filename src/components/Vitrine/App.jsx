d8cfc66820e959db064be068b42d082f3d0fc715
Paulo Eugenio <pauloeugenioreis@msn.com>
2018-11-16 23:24:00 -0400

    substituição de "

--- src/components/Vitrine/App.jsx
+++ src/components/Vitrine/App.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import Slider from 'react-slick';
 import './App.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function OldPrice(props) {
     if (props.price === null) {
